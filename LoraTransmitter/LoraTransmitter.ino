c70361aa97c3a5dcf728dfbd4698f85cbe989d42
Jason Russell <grayhatguy@grayhatguy.com>
2023-04-27 21:16:14 -0400

    Update LoraTransmitter.ino

--- LoraTransmitter/LoraTransmitter.ino
+++ LoraTransmitter/LoraTransmitter.ino
@@ -8,13 +8,14 @@
 
 #include <SPI.h>
 #include <RH_RF95.h>
-
+//
+// check the pins for transmitter build pico with rfm95 TODO
 #define RFM95_CS  10
 #define RFM95_RST 9
 #define RFM95_INT 2
 
 // Change to 868.0 or other frequency, must match RX's freq!
-#define RF95_FREQ 868.0
+#define RF95_FREQ 915.0
 
 // Singleton instance of the radio driver
 RH_RF95 rf95(RFM95_CS, RFM95_INT);
